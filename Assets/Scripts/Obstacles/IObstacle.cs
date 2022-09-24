30c4bbd3891b373a4e877f54d3ddfbaf6e35377c
Francisco Gambero Salinas <frangansal.13@gmail.com>
2022-09-24 18:35:28 +0200

    changed sprites and added new inner wall obstacle and some prefabs

--- Assets/Scripts/Obstacles/IObstacle.cs
+++ Assets/Scripts/Obstacles/IObstacle.cs
@@ -2,7 +2,7 @@
 
 public interface IObstacle {
     enum Type {
-        None, UpperWall, BottomWall, LeftWall, RigthWall, Exit, Entrance, UpperLeftWallCorner, BottomLeftWallCorner, UpperRigthWallCorner, BottomRigthWallCorner
+        None, UpperWall, BottomWall, LeftWall, RigthWall, Exit, Entrance, UpperLeftWallCorner, BottomLeftWallCorner, UpperRigthWallCorner, BottomRigthWallCorner, InnerWall
     }
     Type GetObstacleType();
     bool IsWalkable();
